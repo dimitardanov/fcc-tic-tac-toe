a0852534a20b95998adef56508927b6f92dfd622
Dimitar Danov <dimitardanov@users.noreply.github.com>
2017-10-20 22:05:44 +0300

    Added templates circle, cross svgs and render funcs

--- src/js/renderers/render.js
+++ src/js/renderers/render.js
@@ -1,6 +1,8 @@
 
 var helpers = require('../helpers/helpers.js');
 var emptyCellTempl = require('../templates/empty-cell.hbs');
+var circleTempl = require('../templates/circle.hbs');
+var crossTempl = require('../templates/cross.hbs');
 
 function renderCells(parent, numCells) {
   var cells = helpers.range(numCells);
@@ -8,6 +10,18 @@ function renderCells(parent, numCells) {
   return parent;
 }
 
+function renderCircle(parent) {
+  parent.innerHTML = circleTempl();
+  return parent;
+}
+
+function renderCross(parent) {
+  parent.innerHTML = crossTempl();
+  return parent;
+}
+
 module.exports = {
   emptyCells: renderCells,
+  circle: renderCircle,
+  cross: renderCross
 };
