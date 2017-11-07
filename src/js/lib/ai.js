ef5a3ff6b199903931359265e1acf9004acedc8a
Dimitar Danov <dimitardanov@users.noreply.github.com>
2017-11-07 17:07:37 +0200

    Added hard-coded logic for opening of a perfect game

--- src/js/lib/ai.js
+++ src/js/lib/ai.js
@@ -1,8 +1,29 @@
 
+function round1Player0MoveIndex(board, player) {
+  var rand = Math.random();
+  if (rand < 0.6) {
+    return board.playRandomCorner(player.getId());
+  } else if (rand < 0.9) {
+    return board.playCenter(player.getId());
+  } else {
+    return board.playRandomEdge(player.getId());
+  }
+}
+
+function round1Player1MoveIndex(board, player) {
+  if (board.isCenterCellFree()) {
+    return board.playCenter(player.getId());
+  } else {
+    return board.playRandomCorner(player.getId());
+  }
+}
+
 function makeNextMove(board, player) {
   return board.playRandomCell(player.getId());
 }
 
 module.exports = {
+  round1Player0MoveIndex: round1Player0MoveIndex,
+  round1Player1MoveIndex: round1Player1MoveIndex,
   makeNextMove: makeNextMove
 };
