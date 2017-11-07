e0282f0e71ff5b066d4c3f27555e9ea65bd765b8
Dimitar Danov <dimitardanov@users.noreply.github.com>
2017-11-07 17:05:17 +0200

    Added player methods

--- src/js/lib/player.js
+++ src/js/lib/player.js
@@ -2,11 +2,40 @@
 function Player(id, board) {
   this.id = id;
   this.board = board;
+  this.playedFirstMove = false;
 }
 
 Player.prototype.takeTurn = function(cell) {
+  this.playedFirstMove = true;
   this.board.fillCell(cell, this.id);
-  console.log(this.board);
+};
+
+Player.prototype.setOpponent = function(player) {
+  this.opponent = player;
+};
+
+Player.prototype.getOpponent = function() {
+  return this.opponent;
+};
+
+Player.prototype.hasPlayed = function() {
+  return this.playedFirstMove;
+};
+
+Player.prototype.hasOpponentPlayed = function() {
+  return this.getOpponent().hasPlayed();
+};
+
+Player.prototype.setPlayed = function() {
+  this.playedFirstMove = true;
+};
+
+Player.prototype.getId = function() {
+  return this.id;
+};
+
+Player.prototype.getOpponentId = function() {
+  return this.getOpponent().getId();
 };
 
 module.exports = {
