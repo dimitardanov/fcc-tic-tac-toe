2346cd93f09d31b3b5c879a55e15c4fb52359c50
Dimitar Danov <dimitardanov@users.noreply.github.com>
2017-11-07 16:59:31 +0200

    Added game outcome resolver function

--- src/js/helpers/helpers.js
+++ src/js/helpers/helpers.js
@@ -7,6 +7,64 @@ function range(num) {
   return vals;
 }
 
+function sum(arr) {
+  return arr.reduce(function(acc, el) {
+    if (el == undefined) {
+      return acc;
+    } else {
+      return acc + el;
+    }
+  }, 0);
+}
+
+function createRowValues(arr) {
+  var indexes = [
+    // Rows
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    // Columns
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    // Diagonals
+    [0, 4, 8], [2, 4, 6]
+  ];
+  var rowValues = [];
+  for (var i = 0; i < indexes.length; i++) {
+    var row = indexes[i].reduce(function(acc, ind) {
+      acc.push(arr[ind]);
+      return acc;
+    }, []);
+    rowValues.push(sum(row));
+  }
+  return rowValues;
+}
+
+function isDraw(rowVals) {
+  var drawCriteria = [11, 12, 21];
+  return rowVals.every(function(el) {
+    return drawCriteria.some(function(dc) {
+      return dc == el;
+    });
+  });
+}
+
+function resolveBoard(arr, playerId, opponentId) {
+  var playerWin = 3 * playerId;
+  var opponentWin = 3 * opponentId;
+  var rows = createRowValues(arr);
+  if (isDraw(rows)) {
+    return 0;
+  } else if (rows.includes(playerWin)) {
+    return 1;
+  } else if (rows.includes(opponentWin)) {
+    return -1;
+  } else {
+    return undefined;
+  }
+}
+
 module.exports = {
-  range: range
+  range: range,
+  sum: sum,
+  createRowValues: createRowValues,
+  isDraw: isDraw,
+  resolveBoard: resolveBoard
 };
--- src/js/test/helpers.test.js
+++ src/js/test/helpers.test.js
@@ -15,3 +15,64 @@ describe('range func', function() {
     expect(h.range(4)).to.deep.equal([0,1,2,3]);
   });
 });
+
+describe('sum func', function() {
+  it('should consider undefined as 0', function() {
+    expect(h.sum([undefined, undefined, undefined])).to.equal(0);
+  });
+
+  it('should ignore undefined elements', function() {
+    expect(h.sum([undefined, 10, 1])).to.equal(11);
+  });
+
+  it('should return the sum of the elements of the array', function() {
+    expect(h.sum([1, 10, 10])).to.equal(21);
+  });
+});
+
+describe('createRowValues func', function() {
+  before(function() {
+    this.gameArray = [
+      0, 1, 2,
+      3, undefined, 5,
+      6, 7, 8
+    ];
+    this.rowValues = [
+      3, 8, 21,
+      9, 8, 15,
+      8, 8
+    ];
+  });
+
+  after(function() {
+    delete this.gameArray;
+    delete this.rowValues;
+  });
+
+  it('should return an array with length of 8', function() {
+    expect(h.createRowValues(this.gameArray))
+      .to.be.an('array')
+      .and.to.have.lengthOf(8);
+  });
+
+  it('should return an array of sums of values of possible rows', function() {
+    expect(h.createRowValues(this.gameArray)).to.deep.equal(this.rowValues);
+  });
+});
+
+describe('isDraw func', function() {
+  it('should return true if the array has no other values than 11, 12 or 21',
+    function() {
+      expect(h.isDraw([12, 11])).to.be.true;
+      expect(h.isDraw([11, 21])).to.be.true;
+      expect(h.isDraw([12, 21])).to.be.true;
+      expect(h.isDraw([11, 12, 21, 12])).to.be.true;
+    }
+  );
+
+  it('should return false if array contains values other than 11, 12, 21',
+    function() {
+      expect(h.isDraw([1, 11, 12, 21])).to.be.false;
+    }
+  );
+});
