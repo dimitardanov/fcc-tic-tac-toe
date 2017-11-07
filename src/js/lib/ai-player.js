bc36392ba0931db40fe5248d5552ceba89700652
Dimitar Danov <dimitardanov@users.noreply.github.com>
2017-11-07 17:13:44 +0200

    Updated AI player to use game tree and play perfect game

--- src/js/lib/ai-player.js
+++ src/js/lib/ai-player.js
@@ -1,5 +1,7 @@
 
 var Player = require('./player.js').Player;
+var ai = require('./ai.js');
+var gt = require('./game-tree.js');
 
 function AI(id, board) {
   Player.call(this, id, board);
@@ -8,11 +10,21 @@ function AI(id, board) {
 AI.prototype = Object.create(Player.prototype);
 
 AI.prototype.takeTurn = function() {
-  var turns = this.board.getFreeCellsIndexes();
-  var index = Math.floor(Math.random() * turns.length);
-  var cell = turns[index];
-  Player.prototype.takeTurn.call(this, cell);
-  return cell;
+  if (!this.hasPlayed() && !this.hasOpponentPlayed()) {
+    this.setPlayed();
+    return ai.round1Player0MoveIndex(this);
+  } else if (!this.hasPlayed()) {
+    this.setPlayed();
+    return ai.round1Player1MoveIndex(this.board, this);
+  } else {
+    var gameTree = new gt.GameTree(
+      this.board.duplicateBoard(), this.getOpponent());
+    gt.createGameTree(gameTree);
+    gt.calcMiniMax(gameTree, this);
+    var bestMove = gt.bestMove(gameTree);
+    this.board.fillCell(bestMove, this.getId());
+    return bestMove;
+  }
 };
 
 module.exports = {
